0b9070a9b64095c4aff90d55b42fec6f0864a9c6
Fabien Walle <fabienwalle13@gmail.com>
2025-05-29 11:40:05 +0200

    refactor findthemeswithbooks

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -5,10 +5,16 @@ namespace App\Repository;
 use App\Entity\Book;
 use App\Entity\Theme;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Book>
+ *
+ * @method Book|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Book|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Book[]    findAll()
+ * @method Book[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class BookRepository extends ServiceEntityRepository
 {
@@ -17,25 +23,35 @@ class BookRepository extends ServiceEntityRepository
         parent::__construct($registry, Book::class);
     }
 
-    public function findThemesWithMinBooks(int $min = 10): array
+    public function findThemesWithMinBooks(int $minBooks = 10): array
     {
-        $entityManager = $this->getEntityManager();
-
-        $themeIds = $entityManager->createQueryBuilder()
-            ->select('t.id')
-            ->from(Theme::class, 't')
-            ->join('t.books', 'b')
-            ->groupBy('t.id')
-            ->having('COUNT(b.id) >= :min')
-            ->setParameter('min', $min)
-            ->getQuery()
-            ->getSingleColumnResult();
+        $themeIds = $this->getThemeIdsWithMinBooks($minBooks);
 
         if (empty($themeIds)) {
             return [];
         }
 
-        return $entityManager->createQueryBuilder()
+        return $this->createThemesWithBooksAndAuthorsQuery($themeIds)
+            ->getQuery()
+            ->getResult();
+    }
+
+    private function getThemeIdsWithMinBooks(int $minBooks): array
+    {
+        return $this->createBaseQueryBuilder()
+            ->select('t.id')
+            ->from(Theme::class, 't')
+            ->join('t.books', 'b')
+            ->groupBy('t.id')
+            ->having('COUNT(b.id) >= :min')
+            ->setParameter('min', $minBooks)
+            ->getQuery()
+            ->getSingleColumnResult();
+    }
+
+    private function createThemesWithBooksAndAuthorsQuery(array $themeIds): QueryBuilder
+    {
+        return $this->createBaseQueryBuilder()
             ->select('t', 'b', 'a')
             ->from(Theme::class, 't')
             ->join('t.books', 'b')
@@ -43,9 +59,11 @@ class BookRepository extends ServiceEntityRepository
             ->where('t.id IN (:ids)')
             ->setParameter('ids', $themeIds)
             ->orderBy('t.name', 'ASC')
-            ->addOrderBy('b.title', 'ASC')
-            ->getQuery()
-            ->getResult();
+            ->addOrderBy('b.title', 'ASC');
     }
 
+    private function createBaseQueryBuilder(): QueryBuilder
+    {
+        return $this->getEntityManager()->createQueryBuilder();
+    }
 }
